3474b95aa8da5b91c2e27d96ac1db9d638de567e
profnoah <noah@clarusway.com>
2021-12-18 14:32:10 +0300

    instructor

--- src/components/MyCard.js
+++ src/components/MyCard.js
@@ -3,24 +3,21 @@ import { useNavigate } from "react-router-dom";
 
 const MyCard = (props) => {
   const navigate = useNavigate();
-  const { img, link, text, name } = props.course;
+  const { text, name, img, link } = props.course;
   return (
-    <div>
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src={img} />
-        <Card.Body>
-          <Card.Title>{name}</Card.Title>
-          <Card.Text>{text}</Card.Text>
-          <Button
-            variant="primary"
-            style={{ background: "#C400FF" }}
-            onClick={() => navigate(`/${link}`)}
-          >
-            Details
-          </Button>
-        </Card.Body>
-      </Card>
-    </div>
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={img} />
+      <Card.Body>
+        <Card.Title>{name}</Card.Title>
+        <Card.Text>{text}</Card.Text>
+        <Button
+          style={{ background: "#C400FF" }}
+          onClick={() => navigate(`/${link}`)}
+        >
+          Details
+        </Button>
+      </Card.Body>
+    </Card>
   );
 };
 
